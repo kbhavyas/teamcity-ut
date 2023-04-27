30e1fc39b5d16ea3f3cca63829672029b8a868a9
kbhavyas <99944562+kbhavyas@users.noreply.github.com>
2023-04-27 11:16:00 +0530

    Add files via upload

---
+++ UT-app/UT-app/UT_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  UT_appApp.swift
+//  UT-app
+//
+//  Created by bhavya.kadiyala on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct UT_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
